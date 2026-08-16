3c6d0b06c8ec4f8b6d9d6f65d7620c9a83fa1b08
mintoneko <mobaisilent@gmail.com>
2026-08-16 15:10:00 +0800

    Fix SQLiteAdapter method call in rename script

--- scripts/rename-default-key.js
+++ scripts/rename-default-key.js
@@ -3,7 +3,7 @@ import { SQLiteAdapter } from '../packages/core/src/db-adapter.js';
 const dbPath = process.argv[2] || './data/litedb.db';
 const db = new SQLiteAdapter(dbPath);
 
-const result = db.adapter.prepare(`
+const result = db.prepare(`
   UPDATE "_litedb_keys"
   SET name = '管理员密钥'
   WHERE name = 'Default Admin Key' OR name LIKE '%?%'
